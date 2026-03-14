f37a61a4c022c5ce2499b7b9e89a8fa247cf82af
pixxl <oliverhalenius@yahoo.se>
2026-03-14 16:57:04 +0100

    commit tests change

--- test/test.py
+++ test/test.py
@@ -29,21 +29,17 @@ async def test_project(dut):
     
     #######
     
-    await send(dut, True, 0, 100)
-    await send(dut, True, 1, 101)
-    await send(dut, True, 2, 102)
-    await send(dut, True, 3, 103)
+    # await send(dut, True, 0, 100)
+    # await send(dut, True, 1, 101)
+    # await send(dut, True, 2, 102)
+    # await send(dut, True, 3, 103)
 
-    assert await send(dut, False, 0) == 100
-    assert await send(dut, False, 0) == 100
+    # assert await send(dut, False, 0) == 100
+    # assert await send(dut, False, 0) == 100
 
-    await send(dut, True, 0, 10)
+    # await send(dut, True, 0, 10)
 
-    assert await send(dut, False, 1) == 101
-    assert await send(dut, False, 2) == 102
-    assert await send(dut, False, 3) == 103
-
-    await send(dut, True, 3, 13)
+    # await send(dut, True, 3, 13)
 
     assert await send(dut, False, 0) == 10
 
